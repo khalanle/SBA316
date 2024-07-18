80eaf3a285db2844564b5cdb6e97e7878958bab7
Khalan Le <khalankimle@gmail.com>
2024-07-18 19:59:02 -0400

    commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,7 @@
+// cache elements using getElementById
 const mainTitle = document.getElementById("main-title");
 const drinkTitle = document.getElementById("drink-title");
 const ingredients = document.getElementById("ingredient-list");
 const instructions = document.getElementById("instruction-list");
+// cache element using querySelector
+const listEl = document.querySelector('li');
