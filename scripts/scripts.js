05ca8f1a287ca7b69825d5f3a4808bd6d11225f0
Khalan Le <khalankimle@gmail.com>
2024-07-18 19:28:13 -0400

    commit

---
+++ scripts/scripts.js
@@ -0,0 +1,4 @@
+const mainTitle = document.getElementById("main-title");
+const drinkTitle = document.getElementById("drink-title");
+const ingredients = document.getElementById("ingredient-list");
+const instructions = document.getElementById("instruction-list");
