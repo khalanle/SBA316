9a3f703ba033eaaacbc4efed52e9d222a4969337
Khalan Le <khalankimle@gmail.com>
2024-07-18 23:56:32 -0400

    commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,31 +1,21 @@
 // cache elements using getElementById
-
-// titles and lists
-const mainTitle = document.getElementById("main-title");
-const drinkTitle = document.getElementById("drink-title");
-const ingredients = document.getElementById("ingredient-list");
-const instructions = document.getElementById("instruction-list");
+const mainTitle = document.getElementById('main-title');
+const drinkTitle = document.getElementById('drink-title');
+const ingredients = document.getElementById('ingredient-list');
+const instructions = document.getElementById('instruction-list');
 // comment section
-const cList = document.getElementById("commentList");
-const cInput = document.getElementById("commentInput");
-const cBtn = document.getElementById("addCommentBtn");
+const cList = document.getElementById('commentList');
+const cInput = document.getElementById('commentInput');
+const cBtn = document.getElementById('addCommentBtn');
 // cocktails
-const strawbGin = document.getElementById("straw-gin");
-const oldFashion = document.getElementById("old-fashioned");
-const agavMojito = document.getElementById("agave-mojito");
-const recipeCard = document.getElementsByClassName("cocktail");
-
+const strawbGin = document.getElementById('straw-gin');
+const oldFashion = document.getElementById('old-fashioned');
+const agavMojito = document.getElementById('agave-mojito');
+const recipeCard = document.getElementsByClassName('cocktail');
 // cache element using querySelector
 const listEl = document.querySelector('li');
 
 // change recipe card on click function
-function changeCard(event) {
-    document.cocktail.style.backgroundColor = "red";
-}
-
-// call functions
-recipeCard.addEventListener("click", changeCard);
-
 
 
 // addComment function
@@ -42,7 +32,7 @@ function addComment(event) {
     cInput.value = "";
     // focus the input element
     cInput.focus();
-    cBtn.removeEventListener("click", addComment);
+    cBtn.removeEventListener('click', addComment);
 }
 // call function
-cBtn.addEventListener("click", addComment);
+cBtn.addEventListener('click', addComment);
