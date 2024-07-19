b07a3b09e97c37c88a909e97a12ae80b68c81718
Khalan Le <khalankimle@gmail.com>
2024-07-18 20:20:03 -0400

    commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,5 +3,35 @@ const mainTitle = document.getElementById("main-title");
 const drinkTitle = document.getElementById("drink-title");
 const ingredients = document.getElementById("ingredient-list");
 const instructions = document.getElementById("instruction-list");
+
+const cList = document.getElementById("commentList");
+const cInput = document.getElementById("commentInput");
+const cBtn = document.getElementById("addCommentBtn");
+
 // cache element using querySelector
 const listEl = document.querySelector('li');
+
+
+// create an event handler - function
+function addComment(event) {
+    const newComment = cInput.value;
+    console.log(cInput.value);
+    console.log(event)
+    if (newComment === "") return;
+
+    // add the value to the comment list element
+    const newLi = document.createElement("li");
+    newLi.textContent = newComment;
+    cList.appendChild(newLi);
+
+    // clear the input element
+    cInput.value = "";
+
+    // focus the input element
+    cInput.focus();
+
+    cBtn.removeEventListener("click", addComment);
+}
+
+
+cBtn.addEventListener("click", addComment);
